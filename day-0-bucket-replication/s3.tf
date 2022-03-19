c9d3d880d05ae76f9cb5d5762ba3fe98794e5272
Carlo Mencarelli <carlo.mencarelli@billgo.com>
2022-03-18 22:42:08 -0600

    Bump to 4.6.x provider

--- day-0-bucket-replication/s3.tf
+++ day-0-bucket-replication/s3.tf
@@ -1,3 +1,9 @@
+resource "aws_s3_bucket" "app-artifact-east-2" {
+  bucket = "mencarelli-${random_pet.s3-bucket.id}-east-2"
+
+  provider = aws.east-2
+}
+
 resource "aws_s3_bucket_public_access_block" "app-artifact-east-2" {
   bucket                  = aws_s3_bucket.app-artifact-east-2.id
   block_public_acls       = true
@@ -8,50 +14,75 @@ resource "aws_s3_bucket_public_access_block" "app-artifact-east-2" {
   provider = aws.east-2
 }
 
-resource "aws_s3_bucket" "app-artifact-east-2" {
-  bucket = "mencarelli-${random_pet.s3-bucket.id}-east-2"
-  acl    = "private"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.bucket-encryption-east-2.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
+resource "aws_s3_bucket_versioning" "app-artifact-east-2" {
+  bucket = aws_s3_bucket.app-artifact-east-2.id
+  versioning_configuration {
+    status = "Enabled"
   }
 
-  versioning {
-    enabled = true
-  }
+  provider = aws.east-2
+}
 
-  replication_configuration {
-    role = aws_iam_role.replication[0].arn
+resource "aws_s3_bucket_server_side_encryption_configuration" "app-artifact-east-2" {
+  bucket = aws_s3_bucket.app-artifact-east-2.bucket
 
-    rules {
-      id       = "replication-to-${aws_s3_bucket.app-artifact-west-2.bucket}-west-2"
-      priority = 0
-      status   = "Enabled"
-
-      source_selection_criteria {
-        sse_kms_encrypted_objects {
-          enabled = true
-        }
-      }
-
-      destination {
-        bucket             = aws_s3_bucket.app-artifact-west-2.arn
-        account_id         = local.account_id
-        replica_kms_key_id = aws_kms_key.bucket-encryption-west-2.arn
-
-        access_control_translation {
-          owner = "Destination"
-        }
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.bucket-encryption-east-2.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 
   provider = aws.east-2
+}
+
+resource "aws_s3_bucket_acl" "app-artifact-east-2" {
+  bucket = aws_s3_bucket.app-artifact-east-2.id
+  acl    = "private"
+
+  provider = aws.east-2
+}
+
+resource "aws_s3_bucket_replication_configuration" "app-artifact-east-2" {
+  bucket = aws_s3_bucket.app-artifact-east-2.id
+
+  role = aws_iam_role.replication.arn
+
+  rule {
+    id     = "replication-to-${aws_s3_bucket.app-artifact-west-2.bucket}"
+    status = "Enabled"
+
+    source_selection_criteria {
+      sse_kms_encrypted_objects {
+        status = "Enabled"
+      }
+    }
+
+    destination {
+      bucket  = aws_s3_bucket.app-artifact-west-2.arn
+      account = local.account_id
+      encryption_configuration {
+        replica_kms_key_id = aws_kms_key.bucket-encryption-west-2.arn
+      }
+
+      access_control_translation {
+        owner = "Destination"
+      }
+    }
+  }
+
+
+  depends_on = [
+    aws_s3_bucket_versioning.app-artifact-east-2
+  ]
+
+  provider = aws.east-2
+}
+
+resource "aws_s3_bucket" "app-artifact-west-2" {
+  bucket = "mencarelli-${random_pet.s3-bucket.id}-west-2"
+
+  provider = aws.west-2
 }
 
 resource "aws_s3_bucket_public_access_block" "app-artifact-west-2" {
@@ -64,62 +95,67 @@ resource "aws_s3_bucket_public_access_block" "app-artifact-west-2" {
   provider = aws.west-2
 }
 
-resource "aws_s3_bucket" "app-artifact-west-2" {
-  bucket = "mencarelli-${random_pet.s3-bucket.id}-west-2"
-  acl    = "private"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.bucket-encryption-west-2.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
+resource "aws_s3_bucket_versioning" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.id
+  versioning_configuration {
+    status = "Enabled"
   }
 
-  versioning {
-    enabled = true
-  }
+  provider = aws.west-2
+}
 
-  replication_configuration {
-    role = aws_iam_role.replication[0].arn
+resource "aws_s3_bucket_server_side_encryption_configuration" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.bucket
 
-    rules {
-      id       = "replication-to-${aws_s3_bucket.app-artifact-east-2.bucket}-east-2"
-      priority = 0
-      status   = "Enabled"
-
-      source_selection_criteria {
-        sse_kms_encrypted_objects {
-          enabled = true
-        }
-      }
-
-      destination {
-        bucket             = aws_s3_bucket.app-artifact-east-2.arn
-        account_id         = local.account_id
-        replica_kms_key_id = aws_kms_key.bucket-encryption-east-2.arn
-
-        access_control_translation {
-          owner = "Destination"
-        }
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.bucket-encryption-west-2.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 
   provider = aws.west-2
 }
 
-resource "aws_s3control_multi_region_access_point" "app-artifact" {
-  details {
-    name = "app-artifact"
+resource "aws_s3_bucket_acl" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.id
+  acl    = "private"
 
-    region {
-      bucket = aws_s3_bucket.app-artifact-east-2.id
+  provider = aws.west-2
+}
+
+resource "aws_s3_bucket_replication_configuration" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.id
+
+  role = aws_iam_role.replication.arn
+
+  rule {
+    id     = "replication-to-${aws_s3_bucket.app-artifact-east-2.bucket}"
+    status = "Enabled"
+
+    source_selection_criteria {
+      sse_kms_encrypted_objects {
+        status = "Enabled"
+      }
     }
 
-    region {
-      bucket = aws_s3_bucket.app-artifact-west-2.id
+    destination {
+      bucket  = aws_s3_bucket.app-artifact-east-2.arn
+      account = local.account_id
+      encryption_configuration {
+        replica_kms_key_id = aws_kms_key.bucket-encryption-east-2.arn
+      }
+
+      access_control_translation {
+        owner = "Destination"
+      }
     }
   }
+
+
+  depends_on = [
+    aws_s3_bucket_versioning.app-artifact-west-2
+  ]
+
+  provider = aws.west-2
 }
