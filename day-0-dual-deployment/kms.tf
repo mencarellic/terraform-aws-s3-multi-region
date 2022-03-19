342dbff8d172e036392748a6ec93a059aed0e97b
Carlo Mencarelli <carlo.mencarelli@billgo.com>
2022-03-18 22:59:04 -0600

    Dual Deploy changes

---
+++ day-0-dual-deployment/kms.tf
@@ -0,0 +1,71 @@
+resource "aws_kms_alias" "bucket-encryption-west-2" {
+  name          = "alias/mencarelli-${random_pet.s3-bucket.id}-west-2"
+  target_key_id = aws_kms_key.bucket-encryption-west-2.id
+
+  provider = aws.west-2
+}
+
+resource "aws_kms_alias" "bucket-encryption-east-2" {
+  name          = "alias/mencarelli-${random_pet.s3-bucket.id}-east-2"
+  target_key_id = aws_kms_key.bucket-encryption-east-2.id
+
+  provider = aws.east-2
+}
+
+resource "aws_kms_key" "bucket-encryption-west-2" {
+  description         = "Encryption for mencarelli-${random_pet.s3-bucket.id}-west-2"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.bucket-encryption-kms-policy.json
+
+  provider = aws.west-2
+}
+
+resource "aws_kms_key" "bucket-encryption-east-2" {
+  description         = "Encryption for mencarelli-${random_pet.s3-bucket.id}-east-2"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.bucket-encryption-kms-policy.json
+
+  provider = aws.east-2
+}
+
+data "aws_iam_policy_document" "bucket-encryption-kms-policy" {
+  statement {
+    sid = "Allow access for Key Administrators"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.account_id}:user/terraform"
+      ]
+    }
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:TagResource",
+      "kms:UntagResource",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "Allow use of the key for cross region replication"
+    principals {
+      type        = "AWS"
+      identifiers = [local.account_id]
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt"
+    ]
+    resources = ["*"]
+  }
+}
--- day-0-dual-deployment/s3.tf
+++ day-0-dual-deployment/s3.tf
@@ -1,3 +1,9 @@
+resource "aws_s3_bucket" "app-artifact-east-2" {
+  bucket = "mencarelli-${random_pet.s3-bucket.id}-east-2"
+
+  provider = aws.east-2
+}
+
 resource "aws_s3_bucket_public_access_block" "app-artifact-east-2" {
   bucket                  = aws_s3_bucket.app-artifact-east-2.id
   block_public_acls       = true
@@ -8,15 +14,32 @@ resource "aws_s3_bucket_public_access_block" "app-artifact-east-2" {
   provider = aws.east-2
 }
 
-resource "aws_s3_bucket" "app-artifact-east-2" {
-  bucket = "mencarelli-${random_pet.s3-bucket.id}-east-2"
-  acl    = "private"
-
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "app-artifact-east-2" {
+  bucket = aws_s3_bucket.app-artifact-east-2.id
+  versioning_configuration {
+    status = "Enabled"
   }
 
   provider = aws.east-2
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "app-artifact-east-2" {
+  bucket = aws_s3_bucket.app-artifact-east-2.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.bucket-encryption-east-2.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+
+  provider = aws.east-2
+}
+
+resource "aws_s3_bucket" "app-artifact-west-2" {
+  bucket = "mencarelli-${random_pet.s3-bucket.id}-west-2"
+
+  provider = aws.west-2
 }
 
 resource "aws_s3_bucket_public_access_block" "app-artifact-west-2" {
@@ -29,13 +52,31 @@ resource "aws_s3_bucket_public_access_block" "app-artifact-west-2" {
   provider = aws.west-2
 }
 
-resource "aws_s3_bucket" "app-artifact-west-2" {
-  bucket = "mencarelli-${random_pet.s3-bucket.id}-west-2"
-  acl    = "private"
-
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.id
+  versioning_configuration {
+    status = "Enabled"
   }
+
+  provider = aws.west-2
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.bucket-encryption-west-2.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+
+  provider = aws.west-2
+}
+
+resource "aws_s3_bucket_acl" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.id
+  acl    = "private"
 
   provider = aws.west-2
 }
