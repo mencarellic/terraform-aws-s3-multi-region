7516615dcda8696b1c5dd7ba3c9f3ec3d2e4c98c
Carlo Mencarelli <me@carlo.cloud>
2022-03-28 21:17:33 -0600

    Replication Bucket;

--- day-1-existing-replication/s3.tf
+++ day-1-existing-replication/s3.tf
@@ -124,3 +124,42 @@ data "aws_iam_policy_document" "inventory" {
     }
   }
 }
+
+
+resource "aws_s3_bucket" "app-artifact-west-2" {
+  bucket = "mencarelli-${random_pet.s3-bucket.id}-west-2"
+
+  provider = aws.west-2
+}
+
+resource "aws_s3_bucket_public_access_block" "app-artifact-west-2" {
+  bucket                  = aws_s3_bucket.app-artifact-west-2.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  provider = aws.west-2
+}
+
+resource "aws_s3_bucket_versioning" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  provider = aws.west-2
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "app-artifact-west-2" {
+  bucket = aws_s3_bucket.app-artifact-west-2.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.bucket-encryption-west-2.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+
+  provider = aws.west-2
+}
