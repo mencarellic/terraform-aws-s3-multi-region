4752734613d57f2b3ed5532cdeb0639e6b2deb3b
Carlo Mencarelli <me@carlo.cloud>
2022-03-28 21:18:46 -0600

    KMS

--- day-1-existing-replication/kms.tf
+++ day-1-existing-replication/kms.tf
@@ -13,6 +13,21 @@ resource "aws_kms_key" "bucket-encryption-east-2" {
   provider = aws.east-2
 }
 
+resource "aws_kms_alias" "bucket-encryption-west-2" {
+  name          = "alias/mencarelli-${random_pet.s3-bucket.id}-west-2"
+  target_key_id = aws_kms_key.bucket-encryption-west-2.id
+
+  provider = aws.west-2
+}
+
+resource "aws_kms_key" "bucket-encryption-west-2" {
+  description         = "Encryption for mencarelli-${random_pet.s3-bucket.id}-west-2"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.bucket-encryption-kms-policy.json
+
+  provider = aws.west-2
+}
+
 data "aws_iam_policy_document" "bucket-encryption-kms-policy" {
   statement {
     sid = "Allow access for Key Administrators"
